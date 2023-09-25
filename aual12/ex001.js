f63d975c8192ec5f54ef69a484a00eac87d6f338
Luis Miguel De Souza <luissmigguelsouza@gmail.com>
2023-09-25 22:07:36 +0100

    ex001 aula 12

---
+++ aual12/ex001.js
@@ -0,0 +1,8 @@
+var idade = 22
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18) {
+    console.log('Voto opcional') 
+} else (idade >= 18) {
+    console.log('Voto obrigatório')
+}
