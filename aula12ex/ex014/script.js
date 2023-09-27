3c65f907878c18b874fcaf6133f083f77d3f66a6
Luis Miguel De Souza <luissmigguelsouza@gmail.com>
2023-09-27 22:31:14 +0100

    alterações no exercicio

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,18 @@
-alert('Olá')
+function carregar () {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML(`Agora são ${hora} horas`)
+    
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'pexels-vlada-karpovich-4448846 (1).jpg'
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE
+        img.src = 'pexels-georgedesipris-858241.jpg'
+    } else {
+        //BOA NOITE
+        img.src = 'pexels-pixabay-414144.jpg'
+    }
+}
