fee90cd23ee7278d3700e2a10d6d8a1a0944d4c2
Luis Miguel De Souza <luissmigguelsouza@gmail.com>
2023-10-17 20:09:00 +0100

    exercicio finalizado

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,22 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'Manha.jpg'
+        document.body.style.backgroundColor = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE
+        img.src = 'Tarde.jpg'
+        document.body.style.backgroundColor = '#b9846f'
+    } else {
+        //BOA NOITE
+        img.src = 'Noite.png'
+        document.body.style.backgroundColor = '#515154'
+    }
+}
