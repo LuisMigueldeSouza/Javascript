07a4b390038e88a17c9137318e9d0ec3121b49f5
Luis Miguel De Souza <luissmigguelsouza@gmail.com>
2023-10-18 22:25:56 +0100

    fazendo o JS do exercicio

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,43 @@
-alert('Olá')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero =  ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //CRIANÇA
+            } else if (idade >= 10 && idade < 21) {
+                //JOVEM
+            } else if (idade < 50) {
+                //ADULTO
+            } else {
+                //IDOSO
+            }
+
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //CRIANÇA
+            } else if (idade >= 10 && idade < 21) {
+                //JOVEM
+            } else if (idade < 50) {
+                //ADULTO
+            } else {
+                //IDOSO
+            }
+        } 
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+    }
+}
